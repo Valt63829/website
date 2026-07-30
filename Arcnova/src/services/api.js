e14213c8ad608729b0be77dfbf29ad38f68685cc
Azeem2006 <192472307.simats@saveetha.com>
2026-07-30 11:51:02 +0530

    Update api.js

--- Arcnova/src/services/api.js
+++ Arcnova/src/services/api.js
@@ -1,6 +1,6 @@
 export const sendMessageToAI = async (message) => {
   const response = await fetch(
-    "http://localhost:5000/api/chat",
+    "https://website-7ngm.onrender.com/api/chat",
     {
       method: "POST",
       headers: {
